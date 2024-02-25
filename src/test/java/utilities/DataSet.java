f62de4363906460fdd1128736d694f13153bebf7
imon742 <nazmulislamemon007@gmail.com>
2024-02-25 12:07:27 +0600

    j

--- src/test/java/utilities/DataSet.java
+++ src/test/java/utilities/DataSet.java
@@ -3,7 +3,8 @@ package utilities;
 public class DataSet {
 
     public static Object invalidCredentials(){
-        Object[][] data = { {"standard_user", "secret_", "Epic sadface: Username and password do not match any user in this service" },
+        Object[][] data = {
+                {"standard_user", "secret_", "Epic sadface: Username and password do not match any user in this service" },
                 {"standard_", "secret_sauce", "Epic sadface: Username and password do not match any user in this service" },
                 {"", "secret_sauce", "Epic sadface: Username is required" },
                 {"standard_user", "", "Epic sadface: Password is required" },
